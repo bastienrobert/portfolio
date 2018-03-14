ddd3bead9c3ff32119d607c46a9cf8882162eade
Bastien <bastienrobert@outlook.com>
2018-03-14 18:19:34 +0100

    Add links to copy email

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,4 +3,5 @@ import Favicon from './components/Favicon.js'
 import Loader from './components/Loader.js'
 import OhMyPi from './components/OhMyPi.js'
 import ParticleRepulsion from './components/ParticleRepulsion.js'
+import Email from './components/Email.js'
 Fluffy.create('[data-fluffy-content]')
---
+++ source/javascripts/components/Email.js
@@ -0,0 +1,36 @@
+class Email {
+  constructor () {
+    this.container = document.getElementById('email')
+    this.actions = document.getElementById('email-actions')
+    if (this.container != null) {
+      this.link = this.container.getElementsByClassName('copy')[0]
+      this.copied = this.container.getElementsByClassName('copied')[0]
+      this.content = this.createElement()
+      this.events()
+    }
+  }
+  events () {
+    this.link.addEventListener('click', () => {
+      this.content.select()
+      document.execCommand('copy')
+      this.copied.classList.add('visible')
+      this.actions.style.display = 'none'
+    })
+    this.container.addEventListener('mouseleave', () => {
+      this.copied.classList.remove('visible')
+      this.actions.style.display = 'inline-block'
+    })
+  }
+  createElement () {
+    let input = document.createElement('input')
+    input.type = 'text'
+    input.value = this.link.dataset.email
+    input.style.position = "fixed"
+    input.style.right = "-100%"
+    input.style.top = "-100%"
+    this.link.appendChild(input)
+    return input
+  }
+}
+
+export default new Email
