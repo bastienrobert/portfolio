332ef5111390f8582b4b4b9db73bd88ba2b74dec
Bastien <bastienrobert@outlook.com>
2018-03-14 17:03:27 +0100

    Remove turbolinks, fix CSS bugs and loader refork

--- source/javascripts/components/Loader.js
+++ source/javascripts/components/Loader.js
@@ -3,39 +3,15 @@ class Loader {
     this.loader = document.getElementById('loader')
     if (this.loader != null) {
       this.progress = document.getElementById('progress')
-      Turbolinks.setProgressBarDelay(200)
-      this.event()
+      this.engine()
     }
   }
-  event () {
-    document.addEventListener('turbolinks:load', () => {
-      this.startEvent()
-    })
-  }
-  startEvent () {
-    this.loader.classList.add('visible')
-    this.engine()
-  }
-  stopEvent () {
-    this.loader.classList.remove('visible')
-  }
   engine () {
-    let value = null,
-        turboValue = null,
-        backupValue = 0,
-        width = 0
-    let progress = setInterval(() => {
-      turboValue = Turbolinks.controller.adapter.progressBar.value
-      turboValue == null || turboValue == 1 ? value = backupValue : value = turboValue
-
-      backupValue += .05
-      width += 5
-      this.progress.style.width = width + '%'
-      if (value >= 1 && width >= 100) {
-        clearInterval(progress)
-        this.stopEvent()
-      }
-    }, 70)
+    this.loader.classList.add('visible')
+    let progress = setTimeout(() => {
+      this.loader.classList.remove('visible')
+      clearInterval(progress)
+    }, 2000)
   }
 }
 
